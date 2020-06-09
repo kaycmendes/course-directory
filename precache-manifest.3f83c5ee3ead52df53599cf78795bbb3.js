1bdfd616924c795d9759e8c6664c43f34f96d9d9
Kayc <kaycmendes@gmail.com>
2020-06-09 15:29:16 -0300

    Updates

---
+++ precache-manifest.3f83c5ee3ead52df53599cf78795bbb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3fc6836c38af1d628b53d4bcc2a6f9",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "d7f40e9690cff06d4113",
+    "url": "/course-directory/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "a0e7892c4504a580a062",
+    "url": "/course-directory/static/js/2.25fc61b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/course-directory/static/js/2.25fc61b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7f40e9690cff06d4113",
+    "url": "/course-directory/static/js/main.e6d40dec.chunk.js"
+  },
+  {
+    "revision": "6ce79fa6db6b086a4e3e",
+    "url": "/course-directory/static/js/runtime-main.5dec270c.js"
+  }
+]);
